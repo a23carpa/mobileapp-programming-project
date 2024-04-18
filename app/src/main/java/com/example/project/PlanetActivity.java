458341fe81ba79405c865a358cfbaa8e8387e985
Carl <a23carpa@student.his.se>
2024-04-18 10:51:49 +0200

    Activity och layout för recyckleItem skapas

---
+++ app/src/main/java/com/example/project/PlanetActivity.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlanetActivity extends AppCompatActivity {
+
+    private String name;
+    private int distance;
+    private int size;
+    private int moons;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planet);
+
+
+
+
+
+    }
+}
